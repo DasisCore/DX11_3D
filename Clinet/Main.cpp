2604a8b5ea04392004436e5211d398575853c1de
HeoJaeyoung <gj30347896@gmail.com>
2024-03-11 04:00:48 +0900

    constantBuffer, Camera 실습 완료

--- Clinet/Main.cpp
+++ Clinet/Main.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
 #include "Main.h"
 #include "Engine/Game.h"
-#include "01. TriangleDeme.h"
+#include "01. TriangleDemo.h"
+#include "02. QuadDemo.h"
+#include "03. ConstBufferDemo.h"
+#include "04. CameraDemo.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -13,7 +16,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	desc.width = 800;
 	desc.height = 600;
 	desc.clearColor = Color(0.5f, 0.5f, 0.5f, 0.5f);
-	desc.app = make_shared<TriangleDeme>();
+	desc.app = make_shared<CameraDemo>();
 
 	GAME->Run(desc);
 
