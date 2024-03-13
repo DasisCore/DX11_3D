33833dea6cdc7da37e98164d3deaa7d71aa10882
HeoJaeyoung <gj30347896@gmail.com>
2024-03-14 01:41:46 +0900

    Material

--- Engine/MeshRenderer.cpp
+++ Engine/MeshRenderer.cpp
@@ -5,6 +5,7 @@
 #include "Mesh.h"
 #include "Shader.h"
 #include "Texture.h"
+#include "Material.h"
 
 MeshRenderer::MeshRenderer() : Super(ComponentType::MeshRenderer)
 {
@@ -16,12 +17,35 @@ MeshRenderer::~MeshRenderer()
 
 }
 
+//void MeshRenderer::Update()
+//{
+//	if (_mesh == nullptr || _texture == nullptr || _shader == nullptr)
+//		return;
+//
+//	_shader->GetSRV("DiffuseMap")->SetResource(_texture->GetComPtr().Get());
+//
+//	auto world = GetTransform()->GetWorldMatrix();
+//	RENDER->PushTransformData(TransformDesc{ world });
+//
+//	uint32 stride = _mesh->GetVertexBuffer()->GetStride();
+//	uint32 offset = _mesh->GetVertexBuffer()->GetOffset();
+//
+//	DC->IASetVertexBuffers(0, 1, _mesh->GetVertexBuffer()->GetComPtr().GetAddressOf(), &stride, &offset);
+//	DC->IASetIndexBuffer(_mesh->GetIndexBuffer()->GetComPtr().Get(), DXGI_FORMAT_R32_UINT, 0);
+//
+//	_shader->DrawIndexed(0, 0, _mesh->GetIndexBuffer()->GetCount(), 0, 0);
+//}
+
 void MeshRenderer::Update()
 {
-	if (_mesh == nullptr || _texture == nullptr || _shader == nullptr)
+	if (_mesh == nullptr || _material == nullptr)
 		return;
 
-	_shader->GetSRV("DiffuseMap")->SetResource(_texture->GetComPtr().Get());
+	auto shader = _material->GetShader();
+	if (shader == nullptr)
+		return;
+
+	_material->Update();
 
 	auto world = GetTransform()->GetWorldMatrix();
 	RENDER->PushTransformData(TransformDesc{ world });
@@ -32,5 +56,5 @@ void MeshRenderer::Update()
 	DC->IASetVertexBuffers(0, 1, _mesh->GetVertexBuffer()->GetComPtr().GetAddressOf(), &stride, &offset);
 	DC->IASetIndexBuffer(_mesh->GetIndexBuffer()->GetComPtr().Get(), DXGI_FORMAT_R32_UINT, 0);
 
-	_shader->DrawIndexed(0, 0, _mesh->GetIndexBuffer()->GetCount(), 0, 0);
+	shader->DrawIndexed(0, 0, _mesh->GetIndexBuffer()->GetCount(), 0, 0);
 }
